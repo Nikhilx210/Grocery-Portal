c2dae899aca4634c560c2621c93e9cf3c57d43c3
jai <jaigaur.imp210@gmail.com>
2023-04-22 12:31:36 +0530

    Server_Changes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import colors from 'colors'
 import dotenv from 'dotenv'
 import morgan from 'morgan';
 import connectDB from './config/db.js';
+import authroutes from './routes/authRoute.js';
 //config env
 dotenv.config();
 
@@ -16,6 +17,8 @@ const app =express();
 app.use(express.json())
 app.use(morgan('dev'))
 
+//Routes
+app.use('/api/v1/auth',authroutes);
 //rest api
 app.get('/',(req,res)=>{
     res.send({
