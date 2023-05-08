51cae77d9974669e509c3f32429493da544afe39
Nikhilx210 <nikhilrathee736@gmail.com>
2023-05-08 12:25:10 +0530

    Controller_Addition

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -2,6 +2,21 @@ import slugify from "slugify";
 import productModel from "../models/productModel.js";
 import fs from "fs";
 import categoryModel from "../models/categoryModel.js";
+import braintree from 'braintree'
+import dotenv from 'dotenv'
+import { error } from "console";
+import { response } from "express";
+import orderModel from "../models/orderModel.js";
+//config env
+dotenv.config();
+//payment Gateway
+var gateway = new braintree.BraintreeGateway({
+  environment: braintree.Environment.Sandbox,
+  merchantId: process.env.BRAINTREE_MERCHANT_ID,
+  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+  privateKey: process.env.BRAINTREE_PRIVATE_KEY,
+});
+
 export const createProductController = async (req, res) => {
   try {
     const { name, description, price, category, quantity, shipping } =
@@ -295,5 +310,50 @@ export const productCategoryController =async(req,res)=>{
       message: "Error in Retrieving Category Wise Product",
       error
     });
+  }
+}
+// payment gateway api
+//token
+export const braintreeTokenController =async(req,res)=>{
+  try {
+    gateway.clientToken.generate({},function(err,response){
+      if(err){
+        res.status(500).send(err);
+      }else{
+        res.send(response);
+      }
+    })
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//payment
+export const braintreePaymentController=async(req,res)=>{
+  try {
+    const {cart,nonce}=req.body;
+    let total=0
+    cart.map((i)=> {total+=i.price})
+    let newTransaction=gateway.transaction.sale({
+      amount:total,
+      paymentMethodNonce:nonce,
+      options:{
+        submitForSettlement:true
+      }
+
+    },function(error,result){
+      if(result){
+        const order=new orderModel({
+          products:cart,
+          payment: result,
+          buyer: req.user._id,
+        }).save()
+        res.json({ok:true})
+      }else{
+        res.status(500).send(error);
+      }
+    })
+  } catch (error) {
+    console.log(error);
   }
 }
